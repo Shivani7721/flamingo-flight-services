f45a2fac28d083664a4439500188bd8e0d75c00e
Shivani@7721 <shivanibansod11@gmail.com>
2023-11-27 22:36:19 +0530

    Made useful changes in directory

--- src/main/java/com/flamingo/flightbookings/Flamingo/Flights/Booking/controller/FlightDataController.java
+++ src/main/java/com/flamingo/flightbookings/Flamingo/Flights/Booking/controller/FlightDataController.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class FlightDataController
 {
     @Autowired
