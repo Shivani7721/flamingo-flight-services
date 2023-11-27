1999eebf4f3a343f265ee99d0c6362c69f24d35c
Shivani@7721 <shivanibansod11@gmail.com>
2023-11-27 21:09:03 +0530

    Trying to build on cloud successfully

--- Flamingo-Flights-Booking/src/main/java/com/flamingo/flightbookings/Flamingo/Flights/Booking/controller/FlightDataController.java
+++ Flamingo-Flights-Booking/src/main/java/com/flamingo/flightbookings/Flamingo/Flights/Booking/controller/FlightDataController.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class FlightDataController
 {
     @Autowired
